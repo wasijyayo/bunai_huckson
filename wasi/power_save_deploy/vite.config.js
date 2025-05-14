1d6de72a8749d13a4c0917e2081b3162ed9302d8
wasijyayo <meganekaitai@gmail.com>
2025-05-14 14:43:43 +0900

    パスの修正

--- wasi/power_save_deploy/vite.config.js
+++ wasi/power_save_deploy/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/power_save_deploy/',
+  base: '/bunai_huckson/wasi/power_save_deploy/',
 })
