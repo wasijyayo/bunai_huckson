1da10f90435cacc1a50534997bdd772002024c9f
wasijyayo <meganekaitai@gmail.com>
2025-05-28 15:14:11 +0900

    [fix]ルートディレクトリ変更

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "/bunai_huckson/", // GitHub Pages 用のベースパスを設定
+  build: {
+    outDir: "dist",
+  },
+});
